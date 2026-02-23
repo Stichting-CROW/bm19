cc5439ef9b24ecb22950fde76103155e85bb0a1a
redmer <redmer@users.noreply.github.com>
2026-02-23 15:35:13 +0000

    Initialize new ReSpec repository
    Reason: run setup.yaml after clone by redmer

---
+++ docs/bm19/config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "bm19/bm19",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "crow",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Redmer Kronemeijer",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/bm19",
+  license: "undefined",
+};
